a3f202aaed84d5907373fa8ad4f4991ea8e23c82
rohan209547mourya <rohan.209547@gmail.com>
2023-04-09 23:28:02 +0530

    added database name

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-const MONGO_URI = "mongodb+srv://rohanmourya671:rohan9899@taskplannerclustor.3mryuzl.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = "mongodb+srv://rohanmourya671:rohan9899@taskplannerclustor.3mryuzl.mongodb.net/social_media_db?retryWrites=true&w=majority"
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(MONGO_URI, {
